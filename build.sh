4fd0e08857dbede970e2b0e20d5801944c8de9aa
Marc <mmarc471@gmail.com>
2024-05-22 20:39:08 +0200

    Simpler safer build system

--- build.sh
+++ build.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-VER=555.42.02
-
-wget -c "https://github.com/NVIDIA/nvidia-settings/archive/refs/tags/$VER.zip"
-unzip "$VER.zip"
-mv nvidia-settings-$VER nvidia-settings
-cd nvidia-settings
-	make -j$(nproc)
-cd -
-
-mkdir -p build
-cd build
-	cmake ..
-	make -j$(nproc)
-cd -
-
-cp build/nvoc nvoc
+gcc -c src/main.c
+gcc -o nvoc main.o /usr/lib64/libnvidia-ml.so
+rm main.o
--- src/main.c
+++ src/main.c
@@ -5,32 +5,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static nvmlReturn_t setClock(nvmlDevice_t * device, long offset, nvmlClockType_t type) {
+static nvmlReturn_t setClock(nvmlDevice_t * device, long state, long offset, nvmlClockType_t type) {
 	nvmlClockOffset_t nvmlOffset;
 	memset(&nvmlOffset, 0, sizeof(nvmlOffset));
 	nvmlOffset.version = nvmlClockOffset_v1;
 	nvmlOffset.clockOffsetMHz = offset;
 	nvmlOffset.type = type;//nvmlClockType_t::NVML_CLOCK_GRAPHICS;
 	nvmlPstates_t pstates[] = { NVML_PSTATE_0, NVML_PSTATE_1, NVML_PSTATE_2, NVML_PSTATE_3, NVML_PSTATE_4, NVML_PSTATE_5, NVML_PSTATE_6, NVML_PSTATE_7, NVML_PSTATE_8, NVML_PSTATE_9, NVML_PSTATE_10, NVML_PSTATE_11, NVML_PSTATE_12, NVML_PSTATE_13, NVML_PSTATE_14, NVML_PSTATE_15 };
-
-	for(unsigned long i = 0; i < sizeof(pstates) / sizeof(nvmlPstates_t); i++) {
-		nvmlOffset.pstate = pstates[i];
-	}
+	nvmlOffset.pstate = pstates[state];
 	return nvmlDeviceSetClockOffsets(*device, &nvmlOffset);
+}
+
+void setClocks(nvmlDevice_t * device, long state, long core, long mem, long sm) {
+	nvmlReturn_t result = setClock(device, state, core, NVML_CLOCK_GRAPHICS);
+	if(result != NVML_SUCCESS) {
+		printf("CORE: Failed to set clock: %s PSTATE%ld\n", nvmlErrorString(result), state);
+	} else {
+		printf("Core clock set!\n");
+	}
+
+	result = setClock(device, state, mem, NVML_CLOCK_MEM);
+	if(result != NVML_SUCCESS) {
+		printf("MEM: Failed to set clock: %s PSTATE%ld\n", nvmlErrorString(result), state);
+	} else {
+		printf("Mem clock set!\n");
+	}
+
+	result = setClock(device, sm, state, NVML_CLOCK_SM);
+	if(result != NVML_SUCCESS) {
+		printf("SM: Failed to set clock: %s PSTATE%ld\n", nvmlErrorString(result), state);
+	} else {
+		printf("SM clock set!\n");
+	}
 }
 
 int main(int argc, char ** argv) {
 	if(argc != 4) {
 	usage:
-		printf("format %s [CORE] [MEM] [SM]\n", argv[0]);
+		printf("format %s [PSTATE or -1 for all] [CORE] [MEM] [SM]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
-	long core = strtol(argv[1], NULL, 10);
+	long core = strtol(argv[2], NULL, 10);
 	if(errno != 0) goto usage;
-	long mem = strtol(argv[2], NULL, 10);
+	long mem = strtol(argv[3], NULL, 10);
 	if(errno != 0) goto usage;
-	long sm = strtol(argv[3], NULL, 10);
+	long sm = strtol(argv[4], NULL, 10);
 	if(errno != 0) goto usage;
+	long state = strtol(argv[1], NULL, 10);
+	if(errno != 0) goto usage;
+
+	if(state != -1 && state > 15)
+		goto usage;
 
 	nvmlInit();
 
@@ -41,24 +66,13 @@ int main(int argc, char ** argv) {
 		return EXIT_FAILURE;
 	}
 
-	result = setClock(&device, core, NVML_CLOCK_GRAPHICS);
-	if(result != NVML_SUCCESS) {
-		printf("CORE: Failed to set clock: %s\n", nvmlErrorString(result));
+	if(state == -1) {
+		setClocks(&device, state, core, mem, sm);
 	} else {
-		printf("Core clock set!\n");
+		for(int i = 0; i < 16; i++) {
+			setClocks(&device, i, core, mem, sm);
+		}
 	}
 
-	result = setClock(&device, mem, NVML_CLOCK_MEM);
-	if(result != NVML_SUCCESS) {
-		printf("MEM: Failed to set clock: %s\n", nvmlErrorString(result));
-	} else {
-		printf("Mem clock set!\n");
-	}
 
-	result = setClock(&device, sm, NVML_CLOCK_SM);
-	if(result != NVML_SUCCESS) {
-		printf("SM: Failed to set clock: %s\n", nvmlErrorString(result));
-	} else {
-		printf("SM clock set!\n");
-	}
 }
